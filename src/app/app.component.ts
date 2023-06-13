f5affa6017d44bfb744cdd7738131e6666843b93
alexlamb10 <99216731+alexlamb10@users.noreply.github.com>
2023-06-13 14:18:14 -0600

    Added fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map, startWith } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public form: FormGroup = this._fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: this._fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+    }),
+  });
+  public formUpdates$ = this.form.valueChanges.pipe(
+    startWith(this.form.value),
+    map((value) => {
+      return {
+        ...value,
+        valid: this.form.valid,
+      };
+    })
+  );
+
+  constructor(private _fb: FormBuilder) {}
+}
